d404c9d4c257c44bc26e40d84c1c1095f03fc2e1
BIKTOPBB <BIKTOPBB@gmail.com>
2019-01-08 23:31:27 +0200

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,30 @@ import React, { Component } from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { words: "" };
+    }
+
+    componentDidMount() {
+        let data = this.getWords();
+        this.setState = { words: data };
+    }
+
+    getWords() {
+        fetch("http://localhost:3000/words")
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (data) {
+                data = JSON.parse(data);//парсим JSON, создаем объект
+                console.log(data);
+            })
+            .catch(function (error) {
+                log('Request failed', error)
+            });
+    };
+
     render() {
         return (
             <div>
