d37b25991cf89459d830baece0900ad2a1256b9b
Your NameiBiktop <you@example.combiktopbb@gmail.com>
2020-01-14 18:01:41 +0200

    14.01.2020

--- src/components/mainComponent/userPlaceComponent/groupsListComponent/LearningComponent.jsx
+++ src/components/mainComponent/userPlaceComponent/groupsListComponent/LearningComponent.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import { startLearning } from "../../../../services/requests";
 
 export default function LearningComponent({ learningArr, onSetPage }) {
-
     const [words, setWords] = React.useState(null);
     const [phrase, setPhrase] = React.useState(null);
+
     React.useEffect(() => {
         if (words == null) {
             (async () => {
@@ -13,14 +13,38 @@ export default function LearningComponent({ learningArr, onSetPage }) {
                 setWords(data);
             })();
         }
-        if (words != null && phrase == null) {
-            randomIndex(0, words.length);
-        }
+        // if(words != null && phrase == null) nextWord();
     });
 
-    let randomIndex = (min, max) => {
-        let randomIndex = Math.floor(Math.random() * (max - min) + min);
-        setPhrase(words[randomIndex]);
+    let rand = (min, max) => {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    let nextWord = () => {
+        let randomPercent = rand(0, 100);
+        if(randomPercent>=0 && randomPercent<=10) {
+            let arr = [];
+            for(let i=0; i<words.length; i++){
+                if(words[i].level == 1) arr.push(words[i]);
+            }
+            if(arr.length>0) setPhrase(arr[rand(0, arr.length)]);
+            else nextWord();
+        }
+        if(randomPercent>=11 && randomPercent<=50) {
+            let arr = [];
+            for(let i=0; i<words.length; i++){
+                if(words[i].level == 2) arr.push(words[i]);
+            }
+            if(arr.length>0) setPhrase(arr[rand(0, arr.length)]);
+            else nextWord();
+        }
+        if(randomPercent>=51 && randomPercent<=100) {
+            let arr = [];
+            for(let i=0; i<words.length; i++){
+                if(words[i].level == 3) arr.push(words[i]);
+            }
+            if(arr.length>0) setPhrase(arr[rand(0, arr.length)]);
+            else nextWord();
+        }
     }
 
     if (phrase != null) {
@@ -36,9 +60,9 @@ export default function LearningComponent({ learningArr, onSetPage }) {
                     </div>
                     <div id="menuPlace">
                         <div>
-                            <button className="btn btn-blue" onClick={() => randomIndex(0, words.length)}>Знаю</button><br />
+                            <button className="btn btn-blue" onClick={() => nextWord()}>Знаю</button><br />
                             <button className="btn btn-yellow">Сомневаюсь</button><br />
-                            <button className="btn btn-red">Не знаю</button>
+                            <button className="btn btn-red">Не знаю</button><br />
                             <button className="btn btn-green btn-small" onClick={() => onSetPage('groupList')}><i className="material-icons">arrow_back</i></button>
                         </div>
                     </div>
